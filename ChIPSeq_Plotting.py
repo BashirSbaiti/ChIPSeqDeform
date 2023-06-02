1f272f489c1b4fa23eef9ce590011d99338e780f
BashirSbaiti <bashirsbaiti@gmail.com>
2023-06-02 08:20:05 -0400

    Add files via upload

---
+++ ChIPSeq_Plotting.py
@@ -0,0 +1,235 @@
+import os
+
+import pandas as pd
+import numpy as np
+from Bio import SeqIO
+from matplotlib import pyplot as plt
+import matplotlib as mpl
+import regex as re
+import dill
+
+plt.tight_layout()
+
+PLOT_EXPANDING_SEED = False
+ON_CLUSTER = True
+
+if not ON_CLUSTER:
+    import seaborn as sns
+
+if ON_CLUSTER:
+    mpl.use("Agg")
+else:
+    mpl.use("Qt5Agg")
+
+
+class Peak:
+    def __init__(self, location, intensity):
+        """:param location: tuple of [start, stop)
+           :param intensity: intensity of the peak (float)"""
+        self.location = location
+        self.intensity = intensity
+
+    def __repr__(self):
+        return f"ChIP-Seq peak at {self.location} w/intensity {self.intensity}"
+
+    def getLoc(self):
+        return self.location
+
+    def getIntensity(self):
+        return self.intensity
+
+
+CHROMOSOME = "NC_000913"
+LEN_CHR = 4699742
+
+allPeakSers = list()  # list of series, each series contains all peaks for one ChIP seq file
+
+for file in os.listdir("GEOFiles"):
+    if file.find(".bed") != -1 and file.find(".gz") == -1:
+        name = file[file.find("ChIP") + 5:file.find("summits") - 1]
+        peaks = list()
+        with open(f"GEOFiles/{file}", 'r') as f:
+            for line in f:
+                line = line.replace("\n", "")
+                lst = line.split("\t")
+                chr = lst[0]
+                loc = (int(lst[1]), int(lst[2]))
+                intensity = float(lst[4])
+                if chr == CHROMOSOME:
+                    ipeak = Peak(loc, intensity)
+                    peaks.append(ipeak)
+
+        ser = pd.Series(peaks, name=name, dtype=object)
+        allPeakSers.append(ser)
+
+### Plot number of binding events per gRNA
+
+names = list()
+binds = list()
+for ser in allPeakSers:
+    names.append(ser.name[0:ser.name.find("-")])
+    binds.append(len(ser))
+
+serNames = pd.Series(names, name="name")
+serBinds = pd.Series(binds, name="bind events")
+dfEventsPergRNA = pd.concat([serNames, serBinds], axis=1)
+
+if not ON_CLUSTER:
+    sns.barplot(data=dfEventsPergRNA, x="name", y="bind events", errorbar=('ci', 90))
+    plt.savefig(f"figures/BindingEventsPergRNA")
+
+# Turn series into arrays where index = start loc, intensity = val at index
+# load all these arrays into a list and add replicates
+
+allArrs = list()
+
+for ser in allPeakSers:
+    if ser.name.find("-1") != -1:
+        arr = np.zeros(LEN_CHR, dtype=float)
+    else:
+        arr = allArrs[-1]
+
+    for peak in ser:
+        arr[peak.getLoc()[0]] += peak.getIntensity()
+
+    if ser.name.find("-1") != -1:
+        allArrs.append(arr)
+
+allArrs = [arr / 3 for arr in allArrs]
+# len = 3 [lacZ, AGGAA, ACCCA]
+names = ["lacZ", "AGGAA", "ACCCA"]
+
+allArrsNonzero = list()
+# nonzero version of allArrs, each list element still corresponds to one gRNA but instead represented by a pd series
+# where index = peak location, data = intensity
+
+for i, arr in enumerate(allArrs):
+    x = [ind for ind in range(len(arr)) if arr[ind] != 0]
+    y = [arr[ind] for ind in range(len(arr)) if arr[ind] != 0]
+    allArrsNonzero.append(pd.Series(data=y, index=x, name=names[i]))
+
+### plot distribution of peak intensities for all gRNAs
+for ser in allArrsNonzero:
+    plt.figure()
+    plt.hist([val for val in ser], bins=160)
+    plt.ylabel("Count")
+    plt.xlabel("Intensity")
+    plt.title(f"Distribution of Peak Intensities for {ser.name}")
+    plt.savefig(f"figures/DistIntensities{ser.name}")
+
+### plot binding events across a window for all gRNAs
+
+# window = (3723500, 3727500) # for fig 4a
+window = (3882500, 3886500)  # for fig 4b
+# window = (0, LEN_CHR)
+PlotLargest10All = False
+# (inclusive, exclusive)
+
+for i, ser in enumerate(allArrsNonzero):
+    x = [ind for ind in ser.index if window[0] <= ind < window[1]]  # all nonzero peak positions in window
+    y = [val for ind, val in ser.items() if window[0] <= int(ind) < window[1]]  # all nonzero intensities in window
+    if PlotLargest10All:
+        largest10 = ser.nlargest(10)
+        print(f"Largest 10 binding events for {ser.name}: {largest10}")
+    fig = plt.figure(figsize=(8, 6))
+    plt.title(f"All Binding Events Across the Genome for gRNA {names[i]}")
+    plt.xlabel("Location on MG1655 Genome")
+    plt.ylabel("Intensity")
+    plt.plot(x, y)
+    plt.xticks(range(window[0], window[1], (window[1] - window[0]) // 24), rotation=45)
+    if PlotLargest10All:
+        plt.vlines(largest10.index, 0, largest10.values, colors="r")
+    plt.tight_layout()
+    plt.savefig(f"figures/AllBindingAcrossGenome{names[i]}")
+    # plt.show()
+
+### Plot number of peaks in expanding complementary sequence (fig 4d,f)
+
+if PLOT_EXPANDING_SEED:
+
+    def countPeaksInSpan(peakSer, span):
+        """ :param peakSer: series of peaks where index = location, value = intensity
+            :param span: array like of (inclusive, exclusive) specifying span to count peaks in"""
+        return len([val for ind, val in peakSer.items() if span[0] <= ind < span[1]])
+
+
+    # binding site for all gRNAs = gRNA seq + NGG
+    AGGAAseq = "GGGACCTAAGATTTGAGGAA" + ".GG"
+    ACCCAseq = "TCGAACACACTCTCTACCCA" + ".GG"
+    LacZseq = "TCGTTTTACAACGTCGTGAC" + ".GG"
+
+    bindSites = [LacZseq, AGGAAseq, ACCCAseq]
+    names = ["lacZ", "AGGAA", "ACCCA"]
+
+    ### import referance genome in the same way as main.py
+    RefGenomeFname = "Rostain Supp Info/MG1655 Referance Genome/GCA_000005845.2_ASM584v2_genomic.fna"
+    record = SeqIO.parse(RefGenomeFname, "fasta")
+    refObj = list(record)[0]
+    refGenome = (refObj.seq, refObj.seq.reverse_complement())
+
+    sampleInd = 0
+    for name, bindSite in zip(names, bindSites):
+        seqs = [bindSite[start:] for start in range(len(bindSite) - 1 - 9, len(bindSite) - 2)]
+        fracs = list()
+        # series of differing complementarity to gRNA+PAM
+        for seq in seqs:
+            totalPeaks = 0
+            totalSites = 0
+            # only using fw strand of referance strand because I assume that if they find binding to the reverse strand they map it to the fw
+            for val in re.finditer(seq, str(refGenome[0]), overlapped=True):
+                peaks = countPeaksInSpan(allArrsNonzero[sampleInd], val.span())
+                totalPeaks += peaks
+                totalSites += 1
+            frac = totalPeaks / totalSites
+            fracs.append(frac)
+            print(f"{seq} in {name}: {totalPeaks} peaks, {totalSites} sites, {frac} peaks per site")
+        plt.figure(figsize=(8, 6))
+        plt.plot(seqs, fracs, linestyle="dashed")
+        plt.ylabel("Fraction of Sites With a Peak")
+        plt.xticks(rotation=45)
+        plt.subplots_adjust(bottom=0.15)
+        plt.savefig(f"figures/ExpandingSeed{name}")
+        sampleInd += 1
+
+
+#### Now we investigate deformability stuff
+
+## Plot binding activity over the genome as a rolling average
+
+def getSmoothedPoint(arr, indx, surr=250):
+    """ :returns tuple of mean, standard deviation of surr surrounding base pairs ON EACH SIDE at index indx"""
+    arr = list(arr)  # make sure it's a list so that + operator concatenates instead of adding
+    #TODO: ideas to speed this up, load data into a pandas df and use df.rolling. for rolling we need sd too. to make it work for edge of genome, make a copy of the end of the genome to the beginning
+    upperBound = indx + surr + 1  # exclusive
+    lowerBound = indx - surr  # inclusive
+    if upperBound > len(arr):
+        overflow = (upperBound) - len(arr)
+        region = arr[lowerBound:] + arr[0:overflow]
+    elif lowerBound < 0:
+        region = arr[lowerBound:] + arr[0:upperBound]
+    else:
+        region = arr[lowerBound:upperBound]
+
+    arrReg = np.array(region)
+
+    return arrReg.mean(), arrReg.std()
+
+
+surr = 250
+for arr, name in zip(allArrs, names):
+    means = np.zeros(len(arr))
+    stdevs = np.zeros(len(arr))
+    for i in range(len(arr)):
+        mean, std = getSmoothedPoint(arr, i, surr=surr)
+        means[i] = mean
+        stdevs[i] = std
+
+
+    fig = plt.figure(figsize=(8, 6))
+    plt.title(f"All Binding Events Across the Genome for gRNA {name}, Averaged w/Surrounding {surr}bp")
+    plt.xlabel("Location on MG1655 Genome")
+    plt.ylabel("Intensity")
+    plt.plot(means)
+    plt.fill_between(np.arange(0, len(arr), 1), means-stdevs, means+stdevs, alpha=0.5)
+    plt.tight_layout()
+    plt.savefig(f"figures/AveragedBindingAcrossGenome{name}")
---
+++ main.py
@@ -0,0 +1,219 @@
+from Bio import Entrez
+from Bio import SeqIO
+from Bio.Seq import Seq
+import GEOparse
+import urllib.request
+import gzip
+import shutil
+import regex as re
+import pandas as pd
+import dill
+
+GEOparse.logger.set_verbosity("ERROR")
+
+IMPORTFILES = False
+
+if IMPORTFILES:
+
+    ############# Data Importing ###################
+
+    Entrez.email = "bs407@duke.edu"
+
+
+    def searchDB(db, searchTerm):
+        handle = Entrez.esearch(db=db, term=searchTerm, retmax=200)
+        record = Entrez.read(handle)
+        ids = record['IdList']
+        return ids
+
+
+    #### ChIP-seq data #####
+
+    # Search for ChIP-Seq data on GEO
+    ids = searchDB("gds", "GSE217004")
+    print(f"Found {len(ids)} hits: {ids}.")
+
+    # There are 11 hits:
+    # header file, raw data all together, 3 * triplicates gRNAs (1 targeting, 2 nontargeting (toxic)) processed (9 total)
+
+    # remove header and raw data
+    ids.remove("200217004")
+    ids.remove("100021222")
+
+    # Summarize processed ChIP Seq Data
+    accessionNums = list()
+    for id in ids:
+        handle = Entrez.esummary(db="gds", id=id, report="full")
+        record = Entrez.read(handle)
+        accessionNums.append(record[0]["Accession"])
+
+    # Retrieve data, save to machine
+    for acc in accessionNums:
+        gse = GEOparse.get_GEO(geo=acc, destdir='GEOFiles')
+        suppFTPAdd = gse.metadata['supplementary_file_1'][0]
+        suppFTPAdd = suppFTPAdd.replace("fftp://", "ftp://")  # idk what fftp is but im gonna pretend it doesnt exist
+        name = suppFTPAdd[suppFTPAdd.rfind("/") + 1:]
+        urllib.request.urlretrieve(suppFTPAdd, f"GEOFiles/{name}")
+
+        # unzip downloaded file
+        with gzip.open(f"GEOFiles/{name}", "rb") as fIn:
+            with open(f"GEOFiles/{name}".replace(".gz", ""), "wb") as fOut:
+                shutil.copyfileobj(fIn, fOut)
+
+    ##### Referance Genome #####
+
+    # Search for Genome data
+    ids = searchDB("assembly", "ASM584v2")
+    print(f"Found {len(ids)} hits: {ids}.")
+
+    # There is only one hit
+    id = ids[0]
+
+    # find data file
+    handle = Entrez.esummary(db="assembly", id=id, report="full")
+    record = Entrez.read(handle)
+    FTPUrl = record['DocumentSummarySet']["DocumentSummary"][0]['FtpPath_GenBank']
+    FileName = "GCA_000005845.2_ASM584v2_genomic.fna.gz"
+    FTPUrl += f"/{FileName}"
+
+    # save file
+    name = FTPUrl[FTPUrl.rfind("/") + 1:]
+    urllib.request.urlretrieve(FTPUrl, f"Rostain Supp Info/MG1655 Referance Genome/{FileName}")
+
+    # unzip downloaded file
+    with gzip.open(f"Rostain Supp Info/MG1655 Referance Genome/{FileName}", "rb") as fIn:
+        with open(f"Rostain Supp Info/MG1655 Referance Genome/{FileName}".replace(".gz", ""), "wb") as fOut:
+            shutil.copyfileobj(fIn, fOut)
+
+    ############# Main Body ###################
+
+RefGenomeFname = "Rostain Supp Info/MG1655 Referance Genome/GCA_000005845.2_ASM584v2_genomic.fna"
+
+record = SeqIO.parse(RefGenomeFname, "fasta")  # iterable object with metadata about seq and finally seq object with seq
+refObj = list(record)[0]  # there is only one seq in this file (e coli only have one chromosome)
+# ALL SEQUENCES GO 5 TO 3
+
+refGenome = (refObj.seq, refObj.seq.reverse_complement())
+
+
+# 5 to 3 forward and 5 to 3 reverse strands
+
+
+class GenomicTgtSite:
+
+    def __init__(self, refGenome, PAMLoc, contextLen=500):
+        """ :param refGenome: tuple representing reference genome (5-3 fwd strand, 5-3 reverse strand)
+            :param PAMLoc: location of PAM site in question tuple of (strand num, 5' start incl, 3' stop excl)
+            :param contextLen: number of bp to pull on either side of PAM+tgt site for local context"""
+        self.GENOME = refGenome
+        self.PAMLoc = PAMLoc
+        self.gRNA = self.getgRNA()
+        self.context5, self.context3 = self.getContext(contextLen) if self.getContext(contextLen) is not None else (None, None)
+        self.deform5, self.deform3 = self.getDeform()
+
+    def __repr__(self):
+        PRINT_DEFORM = True
+        VERBOSE_SEQ = False
+        if PRINT_DEFORM:
+            str = f"Target site with PAMLoc = {self.PAMLoc}; gRNA = {self.gRNA}; Context5' = {self.context5}" \
+               f" (len = {len(self.context5[0])} bp, deform = {self.deform5}); Context3' = {self.context3} (len = {len(self.context3[0])} bp, deform = {self.deform3})"
+        else:
+            str = f"Target site with PAMLoc = {self.PAMLoc}; gRNA = {self.gRNA}; Context5' = {self.context5}" \
+                  f" (len = {len(self.context5[0])} bp); Context3' = {self.context3} (len = {len(self.context3[0])})"
+            if VERBOSE_SEQ:
+                str = f"Target site with PAMLoc = {self.PAMLoc}; gRNA = {self.gRNA}; Context5' = {self.context5[0]}" \
+               f" (len = {len(self.context5[0])} bp); Context3' = {self.context3[0]} (len = {len(self.context3[0])} bp)"
+        return str
+
+    def getgRNA(self):
+        """ finds target site sequence given PAM location (read 5-3) on the genome"""
+        strand = self.GENOME[self.PAMLoc[0]]
+        tsStart = self.PAMLoc[1] - 20
+        if tsStart >= 0:
+            ret = Seq(str(strand[self.PAMLoc[1] - 20:self.PAMLoc[1]]).replace("T", "U"))
+        else:
+            ret = Seq(str(strand[tsStart:] + strand[0:self.PAMLoc[1]]).replace("T", "U"))
+        return ret
+
+    def getContext(self, length):
+        """ Find (length) bp fw and rev (fw is defined as PAM containing) on each side of target site"""
+
+        fwStrand = self.GENOME[self.PAMLoc[0]]
+
+        context5Start = self.PAMLoc[1] - 20 - length
+        if context5Start >= 0:
+            context5Fw = fwStrand[context5Start:self.PAMLoc[1] - 20]
+        else: # e. coli genome is ciruclar
+            if self.PAMLoc[1] - 20 >= 0:
+                context5Fw = fwStrand[context5Start:] + fwStrand[0:self.PAMLoc[1] - 20]
+            else:
+                context5Fw = fwStrand[context5Start:self.PAMLoc[1]-20]
+        context5Rv = context5Fw.reverse_complement()
+
+        if len(context5Fw) > length:
+            raise Exception(f"5' is at {len(strand)}")
+
+        context3End = self.PAMLoc[2] + length
+        if context3End <= len(fwStrand) - 1:
+            context3Fw = fwStrand[self.PAMLoc[2]:context3End]
+        else: # e. coli genome is ciruclar
+            overflow = context3End - len(fwStrand)
+            context3Fw = fwStrand[self.PAMLoc[2]:] + fwStrand[0:overflow]
+        context3Rv = context3Fw.reverse_complement()
+
+        if len(context3Fw) > length:
+            raise Exception(f"5' is at {len(strand)}")
+
+        return (context5Fw, context5Rv), (context3Fw, context3Rv)
+
+    def getDeform(self):
+        """ :returns tuple of (avg deform L, avg deform R) over context"""
+
+        deformRefDF = pd.read_csv("DeformabilityParameters_AlbertoPerez.txt", delimiter="\t", index_col=0).T
+
+        strand = self.context5[0]
+        if len(strand) >= 2:
+            deformList = list()  # list of pd series containing deform params for every dinuc in the seq
+            for dinucStart in range(0, len(strand)-1):
+                dinucStop = dinucStart+2
+                dinuc = strand[dinucStart:dinucStop]
+                dinuc = dinuc.reverse_complement() if str(dinuc) not in deformRefDF.columns else dinuc
+                dinucDeform = deformRefDF.loc[:, str(dinuc)]
+                deformList.append(dinucDeform)
+            context5DeformDf = pd.concat(deformList, axis=1)
+            context5DeformAvg = context5DeformDf.mean(axis=1)
+        else:
+            context5DeformAvg = None
+
+        # now do it again for 3'
+        strand = self.context3[0]
+        if len(strand) >= 2:
+            deformList = list()
+            for dinucStart in range(0, len(strand)-1):
+                dinucStop = dinucStart+2
+                dinuc = strand[dinucStart:dinucStop]
+                dinuc = dinuc.reverse_complement() if str(dinuc) not in deformRefDF.columns else dinuc
+                dinucDeform = deformRefDF.loc[:, str(dinuc)]
+                deformList.append(dinucDeform)
+            context3DeformDf = pd.concat(deformList, axis=1)
+            context3DeformAvg = context3DeformDf.mean(axis=1)
+        else:
+            context3DeformAvg = None
+
+        return context5DeformAvg, context3DeformAvg
+
+
+allTs = list()
+
+for strandNum, strand in enumerate(refGenome):
+    for val in re.finditer(r".GG", str(strand), overlapped=True):
+        PAMLoc = (strandNum, val.span()[0], val.span()[1])
+        ts = GenomicTgtSite(refGenome, PAMLoc, contextLen=500)
+        allTs.append(ts)
+
+with open("Rostain Supp Info/MG1655 Referance Genome/allTgtSites", "wb") as f:
+    dill.dump(allTs, f)
+
+# TODO: pickle allTs with dill
+
+# TODO: calculating Tm could be a good control
